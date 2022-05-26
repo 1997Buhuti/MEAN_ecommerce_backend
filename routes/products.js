c1f8bbd576e3201e5a464d1c221069eae0658a7b
Manakal <manakal@randomsoftware.net>
2022-05-26 22:38:28 +0530

    feat: :zap: All the category routes added. Updated product routes with addtional fields

--- routes/products.js
+++ routes/products.js
@@ -11,24 +11,45 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
+router.get('/:id',async(req,res=>{
+  const productList = await product.findById(req.params.id,
+    { 
+      name: req.body.name,
+      description: req.body.description,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.category,
+      category: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    }
+    );
+}))
+
+router.post(`/`, async (req, res) => {
+  //Validating the category
+  const category = await category.findById(req.body.category);
+  if (!category) {
+    return res.status(404).send("Invalid Category cannot add this product");
+  }
   const product = new Product({
     name: req.body.name,
+    description: req.body.description,
     image: req.body.image,
-    countInStock: req.body.countInStock,
+    brand: req.body.brand,
+    price: req.body.category,
+    category: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
 
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
+  newProduct = await product.save();
+  if (!product) {
+    return res.send(500).send("Product cannot be created");
+  }
+  return res.send(newProduct);
 });
 
 module.exports = router;
