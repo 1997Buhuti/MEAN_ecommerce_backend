6cdcdf9875b288c6be3070eccabf7cf2c9a0d886
Manakal <manakal@randomsoftware.net>
2022-09-17 22:22:52 +0530

    fix: :bug: Fixed update category endpoint bug

--- routes/categories.js
+++ routes/categories.js
@@ -1,72 +1,70 @@
-const { Category } = require("../models/category");
-const express = require("express");
+const { Category } = require('../models/category');
+const express = require('express');
 const router = express.Router();
 
 // route to get all the categories
 router.get(`/`, async (req, res) => {
-  const categoryList = await Category.find();
+    const categoryList = await Category.find();
 
-  if (!categoryList) {
-    res.status(500).json({ success: false });
-  }
-  res.send(categoryList);
+    if (!categoryList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(categoryList);
 });
 
 // route to update category
-router.put("/:id", async (req, res) => {
-  const category = await Category.findById(
-    req.params.id,
-    {
-      name: req.body.name,
-      icon: req.body.icon,
-      color: req.body.color,
-    },
-    { new: true }
-  );
-  if (!category) {
-    res.status(400).send("The category cannot created!");
-  }
+router.put('/:id', async (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body.data);
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        { new: true }
+    );
+    if (!category) {
+        res.status(400).send('The category cannot created!');
+    }
 });
 
 //route to get a specific category
-router.get("/:id", async (req, res) => {
-  const category = await Category.findById(req.params.id);
-  res.status(200).send(category);
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    res.status(200).send(category);
 });
 
 //route to create a specific category
-router.post("/", async (req, res) => {
-  let category = new Category({
-    name: req.body.name,
-    icon: req.body.icon,
-    color: req.body.color,
-  });
-  category = await category.save();
-  if (!category)
-    return res.status(404).send({
-      message: "Error couldn't create category.",
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
     });
+    category = await category.save();
+    if (!category)
+        return res.status(404).send({
+            message: "Error couldn't create category."
+        });
 
-  res.send(category);
+    res.send(category);
 });
 
 //route to delete a specific category
-router.delete("/:id", (req, res) => {
-  Category.findByIdAndRemove(req.params.id)
-    .then((category) => {
-      if (category) {
-        return res
-          .status(200)
-          .json({ success: true, message: "Category is deleted" });
-      } else {
-        return res
-          .status(400)
-          .json({ success: false, message: "Category not found" });
-      }
-    })
-    .catch((err) => {
-      return res.status(400).json({ success: false, error: err });
-    });
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({ success: true, message: 'Category is deleted' });
+            } else {
+                return res.status(400).json({ success: false, message: 'Category not found' });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err });
+        });
 });
 
 module.exports = router;
